8d380e82c0afca64a06a788f19274be2fdf11c0a
Drend <byronthyto@gmail.com>
2023-11-09 23:36:42 -0500

    clase 22 finalizada

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,27 +1,26 @@
-import {XCircleIcon} from  '@heroicons/react/24/solid'
+import {XMarkIcon } from  '@heroicons/react/24/solid'
 
 
 
 const OrderCard = props => {
-    const {id, title, price, imgURL, handleDelete } = props; // Corregido de imgeURL a imgURL
+    const {id, title, price, imageUrl, handleDelete } = props // Corregido de imgeURL a imgURL
 
     let renderXMarkIcon
     if(handleDelete){
-        renderXMarkIcon=<XCircleIcon onClick={() => handleDelete(id)} className='w-10 h-8 text-red-500 cursor-pointer'></XCircleIcon>
-
+        renderXMarkIcon=<XMarkIcon  onClick={() => handleDelete(id)} className='w-10 h-8 text-red-500 cursor-pointer'></XMarkIcon>
     }
     return(
         <div className="flex justify-between  items-center mb-4">
             <div className='flex items-center gap-2'>
                 <figure className='w-20 h-20'>
-                    <img className='w-full h-full rounded-lg object-cover' src={imgURL} alt={title} />
+                    <img className='w-full h-full rounded-lg object-cover' src={imageUrl} alt={title} />
                 </figure>
                 <p className='text-sm font-light '>{title}</p>
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>{price}</p>
-            </div>
             {renderXMarkIcon}
+            </div>
         </div>
     )
 }
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,51 +1,48 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
-export const ShoppingCartContext = createContext();
+export const ShoppingCartContext = createContext()
 
-export const ShoppingCartProvider = ({ children }) => {
-    const [count, setCount] = useState(0);
+export const ShoppingCartProvider = ({children}) => {
+  // Shopping Cart · Increment quantity
+  const [count, setCount] = useState(0)
 
-    // vamos validar si el producDetail esta abierto o no
-    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
-    // creamos esta constante para tener esa funcion dentro del context siempre y se encargara de cambiar el valor osea actualizarlo   OPEN Y CLOSE 
-    const openProductDetail = () => setIsProductDetailOpen(true)
-    const closeProductDetail = () => setIsProductDetailOpen(false)
-    
-    
-    // Cerrar o abrir el carrito de compras
-    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
-    const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
-    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+  // Product Detail · Open/Close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
 
-    
-    // producto detail -- Show product
-    // en useStatte ponemos unas llaves por que cuando obtenemos los datos bienen en forma de objeto  
-    const [productToShow, setProductToShow]= useState({})
+  // Checkout Side Menu · Open/Close
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
 
-        //Seccion donde se pone los productos en el carrito
-    const [cartProducts, setcartProducts] = useState([])
-    
-    // Shopping card order --- clase 21
-    const [order, setOrder] = useState([])
+  // Product Detail · Show product
+  const [productToShow, setProductToShow] = useState({})
 
-    return (
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            isProductDetailOpen,
-            openProductDetail,
-            closeProductDetail,
-            productToShow,
-            setProductToShow,
-            cartProducts,
-            setcartProducts,
-            isCheckoutSideMenuOpen,
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            order, // clase 21
-            setOrder, // clase 21
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
+  // Shopping Cart · Add products to cart
+  const [cartProducts, setCartProducts] = useState([])
+
+  // Shopping Cart · Order
+  const [order, setOrder] = useState([])
+
+  return (
+    <ShoppingCartContext.Provider value={{
+      count,
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts,
+      isCheckoutSideMenuOpen,
+      openCheckoutSideMenu,
+      closeCheckoutSideMenu,
+      order,
+      setOrder
+    }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
 }
