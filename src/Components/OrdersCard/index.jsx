1eeff3eab562e78e74ae774daff41bed34802e99
Drend <byronthyto@gmail.com>
2023-11-14 11:28:08 -0500

    Clase 24 finalizada mas estilos en orders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,21 +1,25 @@
-// todo nuevo de la clase 23 
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
-import {XMarkIcon } from  '@heroicons/react/24/solid'
+const OrdersCard = (props) => {
+  const { totalProducts, totalPrice } = props;
+
+  // Obtener la fecha actual
+  const currentDate = new Date();
+
+  // Formatear la fecha como dd.mm.yyyy
+  const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()}`;
+
+  return (
+    <div className="flex justify-around items-center m-2">
+  <p>
+    <span className="mr-10">${totalPrice}</span>
+    <span className="mr-10">{totalProducts} productos</span>
+    <span>{formattedDate}</span>
+  </p>
+</div>
 
 
+  );
+};
 
-const OrdersCard = props => {
-    const {totalProducts,  totalPrice} = props 
-
-    return(
-        <div className="flex justify-between  items-center mb-3 border border-black">
-            <p>
-                <span>01.02.2023</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
-            </p>
-        </div>
-    )
-}
-
-export default OrdersCard
+export default OrdersCard;
