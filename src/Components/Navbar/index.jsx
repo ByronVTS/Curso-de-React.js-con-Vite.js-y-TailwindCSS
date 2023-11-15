0bdd7cfea4e58c7fc276f564dcb21546b2bf2896
Drend <byronthyto@gmail.com>
2023-11-15 08:33:06 -0500

    Clase 27 y 28 finalizada

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,103 +1,119 @@
-import { NavLink } from "react-router-dom";
-// Los componentes NavLink se utilizan en React Router para crear enlaces de navegación que automáticamente aplicarán una clase cuando estén activos (es decir, cuando la ruta actual coincida con la ruta definida en el enlace).
-import { useContext } from "react"
-import { ShoppingCartContext } from "..//../Context"
-import {ShoppingBagIcon} from  '@heroicons/react/24/solid'
-
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
-  //Aqui agregamos los estilos a los nombres de las paginas aqui es donde ponemos una linea debajo a un espacio de 4
-  const activeStyle = 'underline underline-offset-4'
   const context = useContext(ShoppingCartContext)
+  const activeStyle = 'underline underline-offset-4'
+
   return (
-      <nav className='flex justify-between items-center fixed z-10 w-full py-5 top-0 px-8 text-sm font-light bg-white'>
-        {/* Navegación izquierda */}
-      <ul className="flex items-center gap-3">
-        <li className="font-semibold text-lg">
-          <NavLink 
-          to="/"
-          >Shopi</NavLink>
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to='/'>
+            Shopi
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/"
-          className={({isActive}) =>
-            isActive ? activeStyle: undefined
-          }
-          >All</NavLink>
+          <NavLink
+            to='/'
+            onClick={() => context.setSearchByCategory()}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            All
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/Clothes"
-          className={({isActive}) =>
-            isActive ? activeStyle: undefined
-          }
-          >Clothes</NavLink>
+          <NavLink
+            to='/clothes'
+            onClick={() => context.setSearchByCategory('clothes')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Clothes
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/Electronics"
-          className={({isActive}) =>
-          isActive ? activeStyle: undefined
-          }
-          >Electronics</NavLink>
+          <NavLink
+            to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Electronics
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/Furniture"
-          className={({isActive}) =>
-          isActive ? activeStyle: undefined
-          }>Furniture</NavLink>
+          <NavLink
+            to='/furnitures'
+            onClick={() => context.setSearchByCategory('furnitures')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Furnitures
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-            to="/Toys"
-            className={({isActive}) =>
-          isActive ? activeStyle: undefined
-          }>Toys
-        </NavLink>
+          <NavLink
+            to='/toys'
+            onClick={() => context.setSearchByCategory('toys')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Toys
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/Others"
-          className={({isActive}) =>
-          isActive ? activeStyle: undefined
-          }>Others</NavLink>
+          <NavLink
+            to='/others'
+            onClick={() => context.setSearchByCategory('others')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Others
+          </NavLink>
         </li>
       </ul>
-
-      {/* Navegación derecha */}
-      <ul className="flex items-center gap-3">
+      <ul className='flex items-center gap-3'>
         <li className='text-black/60'>
-          Drend 🦇
+          teff@platzi.com
         </li>
         <li>
-          <NavLink 
-          to="/my-orders"
-          className={({isActive}) =>
-            isActive ? activeStyle: undefined
-          }>My Orders</NavLink>
+          <NavLink
+            to='/my-orders'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            My Orders
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/my-account"
-          className={({isActive}) =>
-            isActive ? activeStyle: undefined
-          }>My Account</NavLink>
+          <NavLink
+            to='/my-account'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            My Account
+          </NavLink>
         </li>
         <li>
-          <NavLink 
-          to="/"
-          className={({isActive}) =>
-            isActive ? activeStyle: undefined
-          }>Sign In</NavLink>
+          <NavLink
+            to='/sing-in'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Sign In
+          </NavLink>
         </li>
-        <li className="flex items-center">
-          <ShoppingBagIcon className="h-6 w-6 text-lime-700"/>
-          <div>{context.count}</div>
+        <li className='flex items-center'>
+          <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
+          <div>{context.cartProducts.length}</div>
         </li>
       </ul>
     </nav>
-  );
-};
-export default Navbar;
+  )
+}
+
+export default Navbar
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect} from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -9,10 +9,10 @@ export const ShoppingCartProvider = ({children}) => {
   // Product Detail · Open/Close
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
   const openProductDetail = () => setIsProductDetailOpen(true)
-  
+  const closeProductDetail = () => setIsProductDetailOpen(false)
+
   // Checkout Side Menu · Open/Close
   const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
-  const closeProductDetail = () => setIsProductDetailOpen(false)
   const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
   const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
 
@@ -25,28 +25,54 @@ export const ShoppingCartProvider = ({children}) => {
   // Shopping Cart · Order
   const [order, setOrder] = useState([])
 
-  //Clase 26 -- Get Products 
+  // Get products
   const [items, setItems] = useState(null)
-  //Clase 27 -- vamos a hacer el filtrado por busqueda
   const [filteredItems, setFilteredItems] = useState(null)
 
-  // get products by title 
+  // Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null)
 
-  useEffect(() =>{
-    fetch('https://api.escuelajs.co/api/v1/products')
-    .then(Response =>Response.json())
-    //Lo que hacemos es guardar la data en el setItems
-    .then(data =>setItems(data))
-  },[])
+  // Get products by category
+  const [searchByCategory, setSearchByCategory] = useState(null)
 
-  const filteredItemsByTitle = (items , searchByTitle) => {
-    return items?.filter(item =>  item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
   }
 
   useEffect(() => {
-    if(searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
-  }, [items, searchByTitle])
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
 
   return (
     <ShoppingCartContext.Provider value={{
@@ -69,7 +95,8 @@ export const ShoppingCartProvider = ({children}) => {
       searchByTitle,
       setSearchByTitle,
       filteredItems,
-      setFilteredItems
+      searchByCategory,
+      setSearchByCategory
     }}>
       {children}
     </ShoppingCartContext.Provider>
