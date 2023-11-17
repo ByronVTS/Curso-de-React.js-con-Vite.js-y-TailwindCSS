31f758664ba844dce0b5e539809e094a3e4fdf72
Drend <byronthyto@gmail.com>
2023-11-16 23:53:42 -0500

    cambio de nombre

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -7,7 +7,7 @@ import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import Navbar from '../../Components/Navbar'
-import CheckoutSideMenu from '../../Components/CheckoutSideMenu'
+import CheckoutSideMenu from '../../Components/CheckOutSideMenu/CheckoutSideMenu'
 import './App.css'
 
 const AppRoutes = () => {
