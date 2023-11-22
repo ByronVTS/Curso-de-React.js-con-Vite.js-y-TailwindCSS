24b4da16ce7784252df5129080a385cb52d65fcb
Drend <byronthyto@gmail.com>
2023-11-22 18:47:53 -0500

    Laboratorio 4

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,33 +1,49 @@
+import { useContext } from "react";
 import Layout from "../../Components/Layout";
 import { Link } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 
 function SignIn() {
-  return (
-    <>
-      <Layout>
-        <h1 className="text-center  w-80 mb-6 font-medium text-lg">Bienvenido a Narnia </h1>
-        <div className="flex flex-col w-80">
-          <p>
-            <span className="font-light  text-sm">Email: </span>
-            <span>byronthyt@platzi.com</span>
-          </p>
-          <p>
-            <span className="font-light  text-sm">Password: </span>
-            <span>*********</span>
-          </p>
-          <Link to="/">
-            <button className="bg-black text-white w-full rounded-lg py-3 mt-4 mb-2">Login</button>
-          </Link>
-          <div className="text-center">
-            <a href="/" className="font-light text-ms underline underline-offset-4">Forgot my password</a>
+  const context = useContext(ShoppingCartContext);
+  const account = localStorage.getItem("account");
+  const parsedAccount = account ? JSON.parse(account) : {};
+
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
+  // Esta función devuelve el contenido que parece ser la vista de inicio de sesión
+ 
+    return (
+      <>
+        <Layout>
+          <h1 className="text-center w-80 mb-6 font-medium text-lg">Bienvenido a Narnia</h1>
+          <div className="flex flex-col w-80">
+            <p>
+              <span className="font-light text-sm">Email: </span>
+              <span>{parsedAccount?.email}</span>
+            </p>
+            <p>
+              <span className="font-light text-sm">Password: </span>
+              <span>{parsedAccount?.password}</span>
+            </p>
+            <Link to="/">
+              <button className="bg-black text-white w-full rounded-lg py-3 mt-4 mb-2" disabled={!hasUserAnAccount}>
+                Login
+              </button>
+            </Link>
+            <div className="text-center">
+              <a href="/" className="font-light text-ms underline underline-offset-4" >
+                Forgot my password
+              </a>
+            </div>
+            <button className="border border-black rounded-lg mt-6 py-3" disabled={!hasUserAnAccount}>
+              Sign Up
+            </button>
           </div>
-          <button
-          className="border border-black rounded-lg mt-6 py-3"
-          >Sign Up</button>
-        </div>
-      </Layout>
-    </>
-  );
+        </Layout>
+      </>
+    );
 }
 
 export default SignIn;
